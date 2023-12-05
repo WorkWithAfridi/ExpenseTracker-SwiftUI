fa8abdfc114720c763086ff4f28f33c664f0d2dc
Khondakar Afridi <afridi.khondakar@gmail.com>
2023-12-05 13:45:51 +0600

    Project details changed

---
+++ Expenso/Views/DashboardTabView.swift
@@ -0,0 +1,48 @@
+//
+//  DashboardTabView.swift
+//  CentSage
+//
+//  Created by Christopher Endress on 9/24/23.
+//
+
+import SwiftUI
+
+struct DashboardTabView: View {
+  @Environment(\.managedObjectContext) private var viewContext
+  var logoutAction: () -> Void
+  
+  var body: some View {
+      TabView {
+        TransactionsListView(context: viewContext)
+          .tabItem {
+            Image(systemName: "list.dash")
+            Text("Transactions")
+          }
+        
+        SavingsGoalsListView(context: viewContext)
+          .tabItem {
+            Image(systemName: "star.fill")
+            Text("Savings Goals")
+          }
+        
+        BudgetsListView(context: viewContext)
+          .tabItem {
+            Image(systemName: "dollarsign.circle.fill")
+            Text("Budgets")
+          }
+        
+        SettingsView()
+          .tabItem {
+            Image(systemName: "gearshape")
+            Text("Settings")
+          }
+      }
+      .accentColor(Color("ExpensoGreen"))
+  }
+}
+
+#Preview {
+  DashboardTabView {
+    print("Logout action performed")
+  }
+}
